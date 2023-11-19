8effa40fbebd88faba512a3820b12e0f0d578200
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-11-19 19:30:46 -0300

    adicionando o `opengraph-image` aos products

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,13 +2,17 @@ import { z } from "zod";
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-})
+  APP_URL: z.string().url(),
+});
 
-const parseEnv = envSchema.safeParse(process.env)
+const parseEnv = envSchema.safeParse(process.env);
 
-if(!parseEnv.success) {
-  console.error("Invalid environment variables", parseEnv.error.flatten().fieldErrors)
-  throw new Error("Invalid environment variables")
+if (!parseEnv.success) {
+  console.error(
+    "Invalid environment variables",
+    parseEnv.error.flatten().fieldErrors
+  );
+  throw new Error("Invalid environment variables");
 }
 
-export const env = parseEnv.data
+export const env = parseEnv.data;
